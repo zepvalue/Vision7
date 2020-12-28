4103871b657deca6a941be49a79f13a133a70b53
zepvalue <zepvalue@gmail.com>
2020-12-29 00:26:25 +0100

    added winston for logging purposes

--- build/app.js
+++ build/app.js
@@ -4,6 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
-var app = express_1["default"]();
-app.listen(3000, function () { return console.log("server listening to 3000"); });
+var winston_1 = __importDefault(require("winston"));
+(function () {
+    var app = express_1["default"]();
+    var myformat = winston_1["default"].format.combine(winston_1["default"].format.colorize(), winston_1["default"].format.timestamp(), winston_1["default"].format.align(), winston_1["default"].format.printf(function (info) { return info.timestamp + " " + info.level + ": " + info.message; }));
+    var logger = winston_1["default"].createLogger({
+        transports: [new winston_1["default"].transports.Console({ format: myformat })]
+    });
+    app
+        .listen(3000, function () { return logger.info("✔ Running..."); })
+        .on("error", function (err) {
+        if (err) {
+            logger.error(err);
+            process.exit(1);
+        }
+    });
+})();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
-import express from 'express'
+import express from "express";
+import winston from "winston";
 
-const app = express()
+(function () {
+  const app = express();
 
-app.listen(3000, () => console.log("server listening to 3000"))
+  const myformat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+  );
+
+  const logger = winston.createLogger({
+    transports: [new winston.transports.Console({ format: myformat })],
+  });
+
+  app
+    .listen(3000, () => logger.info("✔ Running..."))
+    .on("error", (err) => {
+      if (err) {
+        logger.error(err);
+        process.exit(1);
+      }
+    });
+})();
